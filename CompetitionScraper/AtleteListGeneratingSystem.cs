efa96a6699b332e2b7ee94e4eab7a0afe2d865ff
Waldemar Krakowiak <wkrak98@gmail.com>
2024-05-06 22:03:25 +0200

    I've improved athlete scraping system so all of the athletes will be downloaded, And then I've improved performance of ListManager class at its peak it got 33 seconds in comparison to earlier 4minutes and 30 seconds

--- CompetitionScraper/AtleteListGeneratingSystem.cs
+++ CompetitionScraper/AtleteListGeneratingSystem.cs
@@ -1,49 +1,75 @@
-﻿using System.Text;
+﻿using HtmlAgilityPack;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CompetitionScraper
 {
     internal class AtleteListGeneratingSystem
     {
-        internal Dictionary<string, string> GetUrls(string clubUrl)
-        {
-            List<string> athletesCategories = ["agegroup=12012", "agegroup=13013", "agegroup=14014", "agegroup=15015", "agegroup=16016", "agegroup=17017", "agegroup=18018", "agegroup=19000", "agegroup=0"];
-            List<string> genderList = ["gender=1", "gender=2"];
-            List<string> poolLength = ["course=LCM", "course=SCM"];
-            string link = clubUrl;
-            string swimrankingsPrefix = "https://www.swimrankings.net/index.php";
-            Dictionary<string, string> swimmers = new Dictionary<string, string>();
-            Parallel.ForEach(athletesCategories, athlete =>
-            {
-                for (int j = 0; j < 2; j++)
-                {
-                    for (int i = 0; i < 2; i++)
-                    {
-                        string changedLink = link.Replace("course=LCM", $"{poolLength[i]}").Replace("gender=1", $"{genderList[j]}").Replace("agegroup=0", $"{athlete}");
-                        var links = ScrapingSystem.Loader(changedLink).DocumentNode.SelectNodes("//td[@class='swimstyle']//a");
-                        Parallel.ForEach(links, (linky) =>
-                        {
-                            var athletes = ScrapingSystem.Loader((swimrankingsPrefix + linky.GetAttributeValue("href", "")).Replace("amp;", ""))
-                                                          .DocumentNode.SelectNodes("//td[@class='fullname']//a");
+        public static string urlPrefix = "https://www.swimrankings.net/index.php";
 
-                            if (athletes != null)
-                            {
-                                foreach (var athlete in athletes)
-                                {
-                                    lock (swimmers)
-                                    {
-                                        if (!swimmers.ContainsKey(athlete.InnerText))
-                                        {
-                                            swimmers.Add(athlete.InnerText, (swimrankingsPrefix + athlete.GetAttributeValue("href", "")).Replace("amp;", ""));
-                                        }
-                                    }
-                                }
-                            }
+        public static async Task<Dictionary<string, string>> GetUrls(string link)
+        {
+            List<string> genders = new List<string> { "gender=1", "gender=2" };
+            List<string> poolLengths = new List<string> { "course=LCM", "course=SCM" };
+
+            var athletes = new ConcurrentDictionary<string, string>();
+
+            List<Task> tasks = new List<Task>();
+            Parallel.ForEach(genders, gender =>
+            {
+                Parallel.ForEach(poolLengths, pool =>
+                {
+                    string modifiedLink = link.Replace("course=LCM", pool).Replace("gender=1", gender);
+                    var distances = ScrapingSystem.Loader(modifiedLink).DocumentNode.SelectNodes("//td[@class='swimstyle']//a");
+                    Parallel.ForEach(distances, distance =>
+                    {
+                        string href = distance.GetAttributeValue("href", "").Replace("amp;", "");
+                        var urlToProcess = $"{AtleteListGeneratingSystem.urlPrefix}{href}";
+                        tasks.Add(Task.Run(() => ProcessUrl(urlToProcess, athletes)));
+                    });
+                });
+            });
+
+            await Task.WhenAll(tasks);
+            Dictionary<string, string> swimmers = new Dictionary<string, string>(athletes);
+
+            return swimmers;
+        }
+
+        public static async Task ProcessUrl(string url, ConcurrentDictionary<string, string> athletes)
+        {
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var page = htmlDocument.DocumentNode.SelectNodes("//td[@class='navigation']")?[9]?.InnerText?
+                .Split(" ");
+
+            if (page != null && page.Length > 0 && double.TryParse(page[page.Length - 1], out double lastPage))
+            {
+                int number = (int)Math.Ceiling(lastPage / 25);
+
+                for (int i = 0; i < number; i++)
+                {
+                    var modifiedHref = url.Replace("firstPlace=1", $"firstPlace={(25 * i) + 1}");
+                    var fullname = htmlDocument.DocumentNode.SelectNodes("//td[@class='fullname']//a");
+
+                    if (fullname != null)
+                    {
+                        Parallel.ForEach(fullname, name =>
+                        {
+                            string athlete = name.InnerText;
+
+                            athletes.AddOrUpdate(DataFormatingSystem.ToTitleString(athlete), AtleteListGeneratingSystem.urlPrefix + name.GetAttributeValue("href", "").Replace("amp;",""), (key, oldValue) => oldValue);
                         });
                     }
                 }
-            });
-            return swimmers;
-
+            }
         }
     }
 }
--- CompetitionScraper/ListManager.cs
+++ CompetitionScraper/ListManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
 using System.Text;
 
 namespace CompetitionScraper
@@ -8,15 +9,7 @@ namespace CompetitionScraper
         internal List<CustomStructure> GetList(string clubUrl)
         {
             AtleteListGeneratingSystem dictionaryManager = new AtleteListGeneratingSystem();
-            Stopwatch sw = Stopwatch.StartNew();
-            sw.Start();
-            Dictionary<string, string> swimmers = dictionaryManager.GetUrls(clubUrl);
-            sw.Stop();
-            Console.WriteLine($"Zakończono działanie stopera: {sw.Elapsed.Seconds:F3}s");
-            foreach (var swimmer in swimmers)
-            {
-                Console.WriteLine(swimmer.Key + " " + swimmer.Value);
-            }
+            Dictionary<string, string> swimmers = AtleteListGeneratingSystem.GetUrls(clubUrl).GetAwaiter().GetResult();
             List<CustomStructure> test = new List<CustomStructure>();
             List<string> helperStringList = new List<string>();
             string liveTimingPrefix = "https://www.swimrankings.net/index.php";
@@ -25,56 +18,62 @@ namespace CompetitionScraper
                 Dictionary<string, int> maximumNumberOfStartsOnCompetition = new Dictionary<string, int>();
                 Dictionary<string, List<string>> listOfStartsOnCompetition = new Dictionary<string, List<string>>();
                 Dictionary<string, Dictionary<string, string>> competitionCityAndDate = new Dictionary<string, Dictionary<string, string>>();
-                var rawAge = ScrapingSystem.Loader(swimmer.Value).DocumentNode.SelectSingleNode("//div[@id='athleteinfo']/div[@id='name']").InnerText;
+                var baseNode = ScrapingSystem.Loader(swimmer.Value);
+                var rawAge = baseNode.DocumentNode.SelectSingleNode("//div[@id='athleteinfo']/div[@id='name']").InnerText;
                 System.Text.RegularExpressions.Match match = System.Text.RegularExpressions.Regex.Match(rawAge, @"\((\d{4})");
                 Console.WriteLine(match.Groups[1].Value);
                 string category = DataFormatingSystem.Category(Convert.ToInt32(match.Groups[1].Value.Trim()));
-                var distances = ScrapingSystem.Loader(swimmer.Value).DocumentNode.SelectNodes("//td[@class='event']//a");
+                var distances = baseNode.DocumentNode.SelectNodes("//td[@class='event']//a");
                 foreach (var distance in distances)
                 {
-                    var events = ScrapingSystem.Loader(liveTimingPrefix + distance.GetAttributeValue("href", "").Replace("amp;", "")).DocumentNode.SelectNodes("//a[@class='time']");
-                    var dates = ScrapingSystem.Loader(liveTimingPrefix + distance.GetAttributeValue("href", "").Replace("amp;", "")).DocumentNode.SelectNodes("//td[@class='date']");
-                    var cities = ScrapingSystem.Loader(liveTimingPrefix + distance.GetAttributeValue("href", "").Replace("amp;", "")).DocumentNode.SelectNodes("//td[@class='city']//a");
+                    var livetimingNode = ScrapingSystem.Loader(liveTimingPrefix + distance.GetAttributeValue("href", "").Replace("amp;", ""));
+                    var events = livetimingNode.DocumentNode.SelectNodes("//a[@class='time']");
+                    var dates = livetimingNode.DocumentNode.SelectNodes("//td[@class='date']");
+                    var cities = livetimingNode.DocumentNode.SelectNodes("//td[@class='city']//a");
                     for (int e = 0; e < events.Count; e++)
                     {
                         if (Convert.ToInt32(dates[e].InnerText.Split("&nbsp;")[2]) == 2024)
                         {
-                            var swimmingDistance = ScrapingSystem.Loader((liveTimingPrefix + events[e].GetAttributeValue("href", "")).Replace("amp;", "")).DocumentNode.SelectNodes("//td")[13].InnerText;
+                            var eventNode = ScrapingSystem.Loader((liveTimingPrefix + events[e].GetAttributeValue("href", "")).Replace("amp;", ""));
+                            var swimmingDistance = eventNode.DocumentNode.SelectNodes("//td")[13].InnerText;
                             if (!swimmingDistance.Contains("Lap"))
                             {
-                                var competitionName = ScrapingSystem.Loader((liveTimingPrefix + events[e].GetAttributeValue("href", "")).Replace("amp;", "")).DocumentNode.SelectNodes("//td[@class='h4']//a")[1];
+                                var competitionName = eventNode.DocumentNode.SelectNodes("//td[@class='h4']//a")[1];
+                                string competitionNameString = competitionName.InnerText;
                                 var competitionDate = ScrapingSystem.Loader((liveTimingPrefix + competitionName.GetAttributeValue("href", "")).Replace("amp;", "")).DocumentNode.SelectNodes("//td[@class='titleRight']")[1].InnerText;
-                                var place = ScrapingSystem.Loader((liveTimingPrefix + events[e].GetAttributeValue("href", "")).Replace("amp;", "")).DocumentNode.SelectNodes("//td")[15].InnerText;
+                                var place = eventNode.DocumentNode.SelectNodes("//td")[15].InnerText;
                                 StringBuilder placeTaken = new StringBuilder();
                                 foreach (char c in place)
                                 {
                                     placeTaken.Append(char.IsDigit(c) ? c : "");
                                 }
-                                string line = $"{DataFormatingSystem.ToTitleString(swimmer.Key)} {placeTaken} miejsce na {DataFormatingSystem.TranslateStroke(swimmingDistance)} " +
-                                              $"{competitionName.InnerText} dnia {DataFormatingSystem.DateTranslation(dates[e].InnerText)}";
-                                if (Convert.ToString(placeTaken) != "" && Convert.ToInt32(placeTaken.ToString()) != 450 && !helperStringList.Contains(line))
+                                string translatedStroke = DataFormatingSystem.TranslateStroke(swimmingDistance);
+                                string translatedDate = DataFormatingSystem.DateTranslation(competitionDate);
+                                string line = $"{swimmer.Key} {placeTaken} miejsce na {translatedStroke} " +
+                                              $"{competitionNameString} dnia {translatedDate}";
+                                if (Convert.ToString(placeTaken) != "" && Convert.ToInt32(placeTaken.ToString()) != 450 && Convert.ToInt32(placeTaken.ToString()) != 800 && !helperStringList.Contains(line))
                                 {
-                                    if (!competitionCityAndDate.ContainsKey(competitionName.InnerText))
+                                    if (!competitionCityAndDate.ContainsKey(competitionNameString))
                                     {
-                                        competitionCityAndDate.Add(competitionName.InnerText, new Dictionary<string, string> { { DataFormatingSystem.DateTranslation(competitionDate), cities[e].InnerText } });
+                                        competitionCityAndDate.Add(competitionNameString, new Dictionary<string, string> { { translatedDate, cities[e].InnerText } });
                                     }
-                                    if (listOfStartsOnCompetition.ContainsKey(competitionName.InnerText))
+                                    if (listOfStartsOnCompetition.ContainsKey(competitionNameString))
                                     {
-                                        listOfStartsOnCompetition[competitionName.InnerText].Add($"{placeTaken} miejsce na {DataFormatingSystem.TranslateStroke(swimmingDistance)}");
+                                        listOfStartsOnCompetition[competitionNameString].Add($"{placeTaken} miejsce na {translatedStroke}");
                                     }
                                     else
                                     {
-                                        listOfStartsOnCompetition.Add(competitionName.InnerText, new List<string> { $"{placeTaken} miejsce na {DataFormatingSystem.TranslateStroke(swimmingDistance)}" });
+                                        listOfStartsOnCompetition.Add(competitionNameString, new List<string> { $"{placeTaken} miejsce na {translatedStroke}" });
                                     }
                                     helperStringList.Add(line);
                                     Console.WriteLine(line);
-                                    if (!maximumNumberOfStartsOnCompetition.ContainsKey(competitionName.InnerText))
+                                    if (!maximumNumberOfStartsOnCompetition.ContainsKey(competitionNameString))
                                     {
-                                        maximumNumberOfStartsOnCompetition.Add(competitionName.InnerText, 1);
+                                        maximumNumberOfStartsOnCompetition.Add(competitionNameString, 1);
                                     }
                                     else
                                     {
-                                        maximumNumberOfStartsOnCompetition[competitionName.InnerText]++;
+                                        maximumNumberOfStartsOnCompetition[competitionNameString]++;
                                     }
                                 }
                             }
@@ -85,7 +84,7 @@ namespace CompetitionScraper
                 {
                     if (listOfStartsOnCompetition.Count > 0)
                     {
-                        CustomStructure cs = new CustomStructure(DataFormatingSystem.ToTitleString(swimmer.Key), listOfStartsOnCompetition, maximumNumberOfStartsOnCompetition.Values.Max(), competitionCityAndDate, category);
+                        CustomStructure cs = new CustomStructure(swimmer.Key, listOfStartsOnCompetition, maximumNumberOfStartsOnCompetition.Values.Max(), competitionCityAndDate, category);
                         test.Add(cs);
                     }
                 }
